15d2572f1bdba27b2ea36fc25d8d211eba86a9f6
Marcos Kauã <marcoskaua12345@gmail.com>
2024-08-14 12:49:16 -0300

    Update Jogo da velha.py
    otimizada a função de verificar vencedor e definir a jogada

--- Jogo da velha.py
+++ Jogo da velha.py
@@ -3,17 +3,14 @@ import random
 matriz = [[' ',' ',' '], [' ',' ',' '], [' ',' ',' ']]
 
 def jogada(pos1, pos2, jogador):
-    if pos1 > 3 or pos1 < 1 or pos2 > 3 or pos2 < 1:
-        return False
-    else:
-        if matriz[pos1-1][pos2-1] == 'X' or matriz[pos1-1][pos2-1] == 'O':
-            return False
-        else:
-            if jogador == 'humano':
+    if pos1 < 4 and pos1 > 0 and pos2 < 4 and pos2 > 0 and matriz[pos1-1][pos2-1] == ' ':
+        if jogador == 'humano':
                 matriz[pos1-1][pos2-1] = 'X'
-            if jogador == 'maquina':
-                matriz[pos1-1][pos2-1] = 'O'
-
+        if jogador == 'maquina':
+            matriz[pos1-1][pos2-1] = 'O'
+    else:
+        return False
+            
 def mostrar_tabuleiro():
     for i in range(3):
         print(f' {matriz[i][0]} | {matriz[i][1]} | {matriz[i][2]} ')
@@ -29,56 +26,35 @@ def jogada_maquina():
             break
 
 def verificar_vencedor():
-    contador_x = 0
-    contador_bola = 0
+    contador_X = 0
+    contador_O = 0
     for i in range(3):
+        contador_X_horizontal = 0
+        contador_O_horizontal = 0
+        contador_X_vertical = 0
+        contador_O_vertical = 0 
         if matriz[i][i] == 'X':
-            contador_x += 1
+            contador_X += 1
         if matriz[i][i] == 'O':
-            contador_bola += 1
-    if contador_x == 3:
-        vencedor_x = True
-        return 'vencedor x'
-    if contador_bola == 3:
-        vencedor_bola = True
-        return 'vencedor bola'
-
-    if matriz[2][0] == 'X' and matriz[1][1] == 'X' and matriz[0][2] == 'X':
-        vencedor_x = True
-        return 'vencedor x'
-    if matriz[2][0] == 'O' and matriz[1][1] == 'O' and matriz[0][2] == 'O':
-        vencedor_bola = True
-        return 'vencedor bola'
-    
-    for i in range(3):
-        contador_x = 0
-        contador_bola = 0
-        for j in range(3):
-            if matriz[i][j] == 'X':
-                contador_x += 1
-            if matriz[i][j] == 'O':
-                contador_bola += 1
-        if contador_x == 3:
-            return 'vencedor x' 
-        if contador_bola == 3:
-            return 'vencedor bola'
-    
-      
-    for i in range(3):
-        contador_x = 0
-        contador_bola = 0  
+            contador_O += 1
         for j in range(3):
             if matriz[j][i] == 'X':
-                contador_x += 1
+                contador_X_vertical += 1
+            if matriz[i][j] == 'X':
+                contador_X_horizontal += 1
+            if matriz[i][j] == 'O':
+                contador_O_horizontal += 1
             if matriz[j][i] == 'O':
-                contador_bola += 1
-        if contador_x == 3:
-            return 'vencedor x'
-        
-        if contador_bola == 3:
-            return 'vencedor bola'
-    
-                                
+                contador_O_vertical += 1
+            if contador_X == 3 or contador_X_vertical == 3 or contador_X_horizontal == 3:
+                return 'vencedor X'
+            if contador_O == 3 or contador_O_horizontal == 3 or contador_O_vertical == 3:
+                return 'vencedor O'    
+    for sinal in 'XO':
+        diagonal_contraria = matriz[2][0] == sinal and matriz[1][1] == sinal and matriz[0][2] == sinal
+        if diagonal_contraria:
+            return (f'vencedor {sinal}')
+                        
 jogadores = ['humano', 'maquina']
 jogador1 = random.choice(jogadores)
 if jogador1 == 'humano':
@@ -110,10 +86,10 @@ while True:
     
     if contador_de_jogadas > 4:
         vencedor = verificar_vencedor()
-        if vencedor == 'vencedor x':
+        if vencedor == 'vencedor X':
             print('Voce venceu')
             break
-        if vencedor == 'vencedor bola':
+        if vencedor == 'vencedor O':
             print('Computador venceu')
             break
     if contador_de_jogadas > 8:
@@ -137,10 +113,10 @@ while True:
     
     if contador_de_jogadas > 4:
         vencedor = verificar_vencedor()
-        if vencedor == 'vencedor x' :
+        if vencedor == 'vencedor X' :
             print('Voce venceu')
             break
-        if vencedor == 'vencedor bola':
+        if vencedor == 'vencedor O':
             print('Computador vencecu')
             break
     if contador_de_jogadas > 8:
